ebac2aa8341f522781a45cbe4a7efec5d8681a81
asadahimeka <smallstar9637@yahoo.co.jp>
2022-08-03 11:21:43 +0800

    feat: add jianshu-clean

---
+++ packages/jianshu-clean/main.ts
@@ -0,0 +1,25 @@
+import './style.less'
+
+type TestFn = (el: HTMLElement) => boolean | null | undefined
+type Callback = (el: HTMLElement) => void
+function waitArriveTest(sel: string, test: TestFn, cb: Callback) {
+  document.arrive(sel, { existing: true }, element => {
+    const el = element as HTMLElement
+    test(el) && cb(el)
+  })
+}
+function waitArrive(sel: string, cb: Callback) {
+  document.arrive(sel, { existing: true }, element => {
+    cb(element as HTMLElement)
+  })
+}
+
+waitArrive('button[type="button"]', el => {
+  if (el.textContent?.includes('阅读全文')) {
+    el.click()
+  }
+  if (el.innerText.includes('关注')) {
+    el.remove()
+  }
+})
+waitArriveTest('div[role=button]', el => el.innerText.includes('抽奖'), el => el.remove())
---
+++ packages/jianshu-clean/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import Userscript from 'vite-plugin-tm-userscript'
+
+export default defineConfig({
+  plugins: [
+    Userscript({
+      entry: 'main.ts',
+      headers: {
+        'name': 'JianshuClean',
+        'namespace': 'https://www.nanoka.top',
+        'author': 'asadahimeka',
+        'description': '简书文章页面界面清理',
+        'source': 'https://github.com/asadahimeka/userscripts',
+        'supportURL': 'https://github.com/asadahimeka/userscripts/issues',
+        'license': 'MIT',
+        'match': 'https://www.jianshu.com/p/*',
+        'require': 'https://lib.baomitu.com/arrive/2.4.1/arrive.min.js',
+        'run-at': 'document-start',
+      },
+    }),
+  ],
+})
