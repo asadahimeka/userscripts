aa9bec74a3ac1253ddea1d14d6a50f189d9cc758
asadahimeka <smallstar9637@yahoo.co.jp>
2022-08-26 19:31:49 +0800

    release gcs-cal-only v0.1.1

--- packages/gcs-cal-only/dist/gcs-cal-only.user.js
+++ packages/gcs-cal-only/dist/gcs-cal-only.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name              原神观测枢日历放大
-// @version           0.1.0
+// @version           0.1.1
 // @description       原神观测枢素材活动日历放大到整屏（1920*1080）
 // @author            asadahimeka
 // @namespace         https://www.nanoka.top
@@ -8,7 +8,7 @@
 // @match             https://bbs.mihoyo.com/ys/obc/?bbs_presentation_style=no_header&cal=1
 // @source            https://github.com/asadahimeka/userscripts
 // @supportURL        https://github.com/asadahimeka/userscripts/issues
-// @run-at            document-start
+// @run-at            document-end
 // @grant             GM_addStyle
 // ==/UserScript==
 
@@ -29,16 +29,18 @@
   `);
 
   function run() {
-    const cols = document.querySelectorAll(".cal-pc__row--event .cal-pc__col .cal-pc__col:not(.cal-pc__col--event)");
-    for (const el of cols) {
-      const w = parseFloat(el.style.width);
-      if (w)
-        el.style.width = `${w * 2.6041666666666665}px`;
-    }
-    const right = document.querySelector(".home-channel--calendar .home-channel__title .home-channel__right");
-    if (!right)
-      return;
-    right.innerHTML = '<a href="https://bbs.mihoyo.com/ys/obc/" class="channel-more">\u66F4\u591A<span>\u5185\u5BB9</span></a>';
+    setTimeout(() => {
+      const cols = document.querySelectorAll(".cal-pc__row--event .cal-pc__col .cal-pc__col:not(.cal-pc__col--event)");
+      for (const el of cols) {
+        const w = parseFloat(el.style.width);
+        if (w)
+          el.style.width = `${w * 2.6041666666666665}px`;
+      }
+      const right = document.querySelector(".home-channel--calendar .home-channel__title .home-channel__right");
+      if (!right)
+        return;
+      right.innerHTML = '<a href="https://bbs.mihoyo.com/ys/obc/" class="channel-more">\u66F4\u591A<span>\u5185\u5BB9</span></a>';
+    }, 500);
   }
   addEventListener("load", run);
 })();
